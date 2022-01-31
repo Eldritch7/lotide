1b3c3c68e99765438e76b5ff985ff0c54e2eb301
Sara <parakeet_22@yahoo.com>
2022-01-31 15:57:03 -0500

    made a function that tests if two arguments are equal - it has 4 lines testing the code right now

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅👌 Assertion Passed: ${actual} === ${expected}.`);
+  } else if (actual !== expected) {
+    return console.log(`⛔🔥 Assertion Failed: ${actual} !== ${expected}.`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(0, 4);
+assertEqual("hat", "hat");
