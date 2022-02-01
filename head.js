b34f47951fb725aa8e1ee3b0d10633f16f3b76c8
Sara <parakeet_22@yahoo.com>
2022-01-31 19:03:43 -0500

    A function that print the head of an array and is tested with assertEqual

---
+++ head.js
@@ -0,0 +1,22 @@
+//assertEqual Function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅👌 Assertion Passed: ${actual} === ${expected}.`);
+  } else if (actual !== expected) {
+    return console.log(`⛔🔥 Assertion Failed: ${actual} !== ${expected}.`);
+  }
+};
+
+//Head Function
+const head = function(array) {
+  return array [0];
+};
+
+
+
+//Test Code:
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([1]), 1);
