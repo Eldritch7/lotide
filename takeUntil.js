7bd25d91a1824ffb2408a86aaebfa870d8f44593
Sara <parakeet_22@yahoo.com>
2022-02-09 00:37:04 -0500

    a function that uses a callback function which returns true or false to stop pushing to an array at a specific value

---
+++ takeUntil.js
@@ -0,0 +1,59 @@
+//EQUAL ARRAY
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i <= array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+
+  }
+  return true;
+  
+};
+
+//  ASSERT ARRAYS EQUAL
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return console.log(`✅👌 Assertion Passed: ${array1} === ${array2}.`);
+  } else if (!eqArrays(array1, array2)) {
+    return console.log(`⛔🔥 Assertion Failed: ${array1} !== ${array2}.`);
+  }
+};
+
+x => x === ',';
+x => x < 0;
+
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (const el of array) {
+    if (callback(el)) {
+      break;
+      
+    } else {
+      results.push(el);
+    }
+
+
+  } return results;
+  
+  
+};
+
+
+//Test Code
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
