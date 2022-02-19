fc9118a7be97fa67f27f9409943e3a01846679a3
Sara <parakeet_22@yahoo.com>
2022-02-19 15:56:08 -0500

    cleaned up test

--- head.js
+++ head.js
@@ -13,12 +13,3 @@ const head = function(array) {
  
   }
 };
-
-
-
-//Test Code:
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([1]), 1);
