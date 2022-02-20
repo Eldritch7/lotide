8a122864943e52162722949115c050e63805d69d
Sara <parakeet_22@yahoo.com>
2022-02-19 19:09:19 -0500

    changed to mocha and chai tests for  middleTest.js and tailTest.js and kept the old code

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,26 +1,46 @@
-//Import
-const assertEqual = require('../assertEqual');
-const tail = require('../tail.js');
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+  it("returns [2,3,4] for [1,2,3,4]", () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3,4]); 
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []); 
+  });
+});
 
 
-//Test Case: It can't compare arrays
-console.log("assertEqual won't work with arrays, see below", tail([1,2,3,4,5]));
-assertEqual(tail([1,2,3,4,5]), [2,3,4,5]);
+
+// //Import
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail.js');
 
 
-//Test Case: Empty array and array with one element
-const empty = [];
+// //Test Case: It can't compare arrays
+// console.log("assertEqual won't work with arrays, see below", tail([1,2,3,4,5]));
+// assertEqual(tail([1,2,3,4,5]), [2,3,4,5]);
 
-console.log("single number array",tail([1]));
-console.log('empty array', tail(empty));
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// //Test Case: Empty array and array with one element
+// const empty = [];
 
-// Test Case: Check the returned array elements - it works if you use it only for specific elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// console.log("single number array",tail([1]));
+// console.log('empty array', tail(empty));
+
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// // Test Case: Check the returned array elements - it works if you use it only for specific elements
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
