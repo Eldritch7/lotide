b6171d3b4582a4581b28c38b51e9580fa430584b
Sara <parakeet_22@yahoo.com>
2022-02-20 02:25:38 -0500

    final update

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -9,6 +9,6 @@ const without = require('../without');
 
 
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
+const gone = without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(gone, ["hello", "world"]);
